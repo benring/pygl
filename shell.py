493b501825314f43ab6ff178d2d90b6a20b6a805
benring <benring96@gmail.com>
2020-12-28 16:27:42 -0500

    working

--- shell.py
+++ shell.py
@@ -1,68 +1,84 @@
+import logging
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+
 import OpenGL
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-from keycallback import *
+from util import *
+from shapes import *
+from lights import Light
 
+PIXEL_PER_UNIT = 32
 
-w, h = 500,500
+state = Singleton()
 
-# ---Section 1---
-def square(x=0, y=0, d=1):
-    # We have to declare the points in this sequence: bottom left, bottom right, top right, top left
-    glBegin(GL_QUADS) # Begin the sketch
-    glColor3f(0.5, 0.0, 1.0)
-    glVertex2f(x, y) # Coordinates for the bottom left point
-    glVertex2f(x+d, y) # Coordinates for the bottom right point
-    glColor3f(1.0, 0.5, 1.0)
-    glVertex2f(x+d, y+d) # Coordinates for the top right point
-    glVertex2f(x, y+d) # Coordinates for the top left point
-    glEnd() # Mark the end of drawing
+def ReSizeGLScene(Width, Height):
+	global state
 
-# This alone isn't enough to draw our square
-
-
-# A general OpenGL initialization function.  Sets all of the initial parameters.
-def InitGL(Width, Height):				# We call this right after our OpenGL window is created.
-	glClearColor(0.0, 0.0, 0.0, 0.0)	# This Will Clear The Background Color To Black
-	glClearDepth(1.0)					# Enables Clearing Of The Depth Buffer
-	glDepthFunc(GL_LESS)				# The Type Of Depth Test To Do
-	glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
-	glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
-
+	x, y = Width/(2.*PIXEL_PER_UNIT), Height/(2.*PIXEL_PER_UNIT)
+	z = 2*max(x, y) #approx z
+	state.x, state.y, state.z = x, y, z
+	# state.bounds.set(x, y, z)
+	logging.info("Window: {} x {} ".format(Width, Height))
+	if Height == 0:           # Prevent A Divide By Zero If The Window Is Too Small
+		Height = 1
+	glViewport(0, 0, Width, Height)   # Reset The Current Viewport And Perspective Transformation
 	glMatrixMode(GL_PROJECTION)
-	glLoadIdentity()					# Reset The Projection Matrix
-										# Calculate The Aspect Ratio Of The Window
-	gluPerspective(45.0, float(Width)/float(Height), 0.1, 1.0)
-
-	glMatrixMode(GL_MODELVIEW)
-
-# for reshaping the window
-def reshape(w, h):
-	print('w,h = {}, {}\n'.format(w,h))
-	glViewport(0, 0, w, h)
-	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity()
-	gluPerspective(45.0, w/h, -100, 100.0)
+	glOrtho (-x, x, -y, y, -z, z)
 	glMatrixMode(GL_MODELVIEW)
 
-def showScreen():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Remove everything from screen (i.e. displays all white)
-    glLoadIdentity() # Reset all graphic/shape's position
-    square(0, 0, d=10) # Draw a square using our function
-    glutSwapBuffers()
+def InitGL(Width, Height):        # We call this right after our OpenGL window is created.
 
-#---Section 3---
-keyboard = keyCallBack()
-glutInit()
-glutInitDisplayMode(GLUT_RGBA) # Set the display mode to be colored
-glutInitWindowSize(640, 480)   # Set the w and h of your window
-glutInitWindowPosition(50, 50)   # Set the position at which this windows should appear
-wind = glutCreateWindow("My OpenGL Window") # Set a window title
-glutDisplayFunc(showScreen)
-glutIdleFunc(showScreen) # Keeps the window open
-# glutReshapeFunc(reshape)
-glutKeyboardFunc(keyboard.keyPressed)  # Registered keyboard callback function
-InitGL(640, 480)
-glutMainLoop()  # Keeps the above created window displaying/running in a loop
+	glClearColor(0.0, 0.0, 0.0, 0.0)  # This Will Clear The Background Color To Black
+	glClearDepth(1.0)         # Enables Clearing Of The Depth Buffer
+	glDepthFunc(GL_LESS)        # The Type Of Depth Test To Do
+	glEnable(GL_DEPTH_TEST)       # Enables Depth Testing
+	glShadeModel(GL_SMOOTH)       # Enables Smooth Color Shading
+	glEnable(GL_NORMALIZE)
+ 
+	#Set up material properties
+	materials['brass'].set()
+
+	# Set up lighting
+	Light.default()
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+
+	ReSizeGLScene(Width, Height)
+
+def DrawGLScene():
+	global state, lite0
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glLoadIdentity()
+
+	square()
+
+	glutSwapBuffers()
+
+def main():
+
+	# pass arguments to init
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+
+	# get a 640 x 480 window
+	glutInitWindowSize(640, 480)
+	glutInitWindowPosition(0, 0)
+	window = glutCreateWindow("My OpenGL Canvas")
+
+	glutDisplayFunc(DrawGLScene)    # Register the drawing function with glut
+	glutIdleFunc(DrawGLScene)   # When we are doing nothing, redraw the scene.
+	glutReshapeFunc(ReSizeGLScene)
+	# glutKeyboardFunc(state.keyPressed)  # Registered keyboard callback function
+	# glutSpecialFunc(state.specialKey)
+	# glutMouseFunc(state.mouse)
+
+	InitGL(640, 480)      # Initialize our window.
+	glutMainLoop()        # Start Event Processing Engine
+
+
+main()
